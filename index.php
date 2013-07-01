f1c1a66cac4f5536c0b8001341b40d0525287811
Matt Gibbs <mgibbs189@gmail.com>
2013-07-01 11:54:21 -0400

    Tabs to spaces

--- index.php
+++ index.php
@@ -21,43 +21,43 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, see <http://www.gnu.org/licenses/>.
 */
 
-$rsp = new RSB();
+$rsb = new RSB();
 
 class RSB
 {
-	function __construct() {
-		add_action('init', array($this, 'init'));
-	}
+    function __construct() {
+        add_action('init', array($this, 'init'));
+    }
 
-	function init() {
-		//add_action('admin_menu', array($this, 'admin_menu'));
-		add_action('register_form', array($this, 'registration_page'));
-		add_action('registration_errors', array($this, 'check_registration'), 10, 3);
-	}
+    function init() {
+        //add_action('admin_menu', array($this, 'admin_menu'));
+        add_action('register_form', array($this, 'registration_page'));
+        add_action('registration_errors', array($this, 'check_registration'), 10, 3);
+    }
 
-	/*
-	function admin_menu() {
-		add_submenu_page('options-general.php', 'Registration Spam Blocker', 'Registration Spam Blocker', 'manage_options', 'rsb', array($this, 'admin_page'));
-	}
+    /*
+    function admin_menu() {
+        add_submenu_page('options-general.php', 'Registration Spam Blocker', 'Registration Spam Blocker', 'manage_options', 'rsb', array($this, 'admin_page'));
+    }
 
-	function admin_page() {
-		echo 'hello world';
-	}
-	*/
+    function admin_page() {
+        echo 'hello world';
+    }
+    */
 
-	function registration_page() {
-	?>
-		<label for="validate"><a href="https://www.google.com/search?q=what+is+the+capital+of+Virginia" target="_blank">What is the capital of Virginia?</a></label>
-		<input type="text" name="validate" id="validate" class="input" size="20" />
-	<?php
-	}
+    function registration_page() {
+    ?>
+        <label for="validate"><a href="https://www.google.com/search?q=what+is+the+capital+of+Virginia" target="_blank">What is the capital of Virginia?</a></label>
+        <input type="text" name="validate" id="validate" class="input" size="20" />
+    <?php
+    }
 
-	function check_registration($errors, $user_login, $user_email) {
-		$answer = $_POST['validate'];
-		if ('richmond' != strtolower($answer)) {
-			$errors->add('validation_failed', '<strong>ERROR</strong>: The validation code is incorrect.');
-		}
+    function check_registration($errors, $user_login, $user_email) {
+        $answer = $_POST['validate'];
+        if ('richmond' != strtolower($answer)) {
+            $errors->add('validation_failed', '<strong>ERROR</strong>: The validation code is incorrect.');
+        }
 
-		return $errors;
-	}
+        return $errors;
+    }
 }
